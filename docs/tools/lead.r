06264a9daaf5644d4e92c2ab829436aaa7bfc32d
Rufibach <rufiback@emea.roche.com>
2022-10-27 17:30:52 +0200

    update

--- docs/tools/lead.r
+++ docs/tools/lead.r
@@ -1,7 +1,12 @@
 lead <- function(subteam, type = 1){
   l1 <- (email %>% filter(Lead_TF == subteam))
-  l2 <- paste((l1 %>% select(Firstname, Lastname)), collapse = " ")
-  if (type == 1){l3 <- paste(l2, " (", (l1 %>% select(Institution)), ")", sep = "")}
-  if (type == 2){l3 <- paste(l2, ", ", (l1 %>% select(Institution)), sep = "")}
-  return(l3)
+  l2 <- as.data.frame(l1 %>% select(Firstname, Lastname, Institution))
+  
+  if (type == 1){
+    for (k in 1:nrow(l2)){l2[k, 4] <- paste(paste(l2[k, 1:2], collapse = " "), " (", l2[k, 3], ")", sep = "")}}
+
+  if (type == 2){
+    for (k in 1:nrow(l2)){l2[k, 4] <- paste(paste(l2[k, 1:2], collapse = " "), ", ", l2[k, 3], sep = "")}}
+  
+  return(l2[, 4])
 }
