8b9612856aee2f19d8ebb4a351bed0384f99f52c
Kaspar Rufibach <kaspar.rufibach@roche.com>
2023-02-18 11:49:05 +0100

    update

--- tools/lead.r
+++ tools/lead.r
@@ -1,5 +1,8 @@
-lead <- function(subteam, type = 1){
+lead <- function(subteam, type = 1, active = 1){
   l1 <- (email %>% filter(Lead_TF == subteam))
+  
+  if (active == 3){l1 <- (email %>% filter(Lead == subteam))}
+  
   l2 <- as.data.frame(l1 %>% select(Firstname, Lastname, Institution))
   
   if (type == 1){
--- tools/obj.r
+++ tools/obj.r
@@ -1,4 +1,4 @@
-obj <- function(subteam, type = 1){
+obj <- function(subteam, type = 1, active = 1){
   l1 <- (objectives %>% filter(taskforce == subteam))[, 2]
   return(l1)
 }
